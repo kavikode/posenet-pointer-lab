7f600a2e4a096f40e5833cfae997544d01ed48af
Oz Ramos <labofoz@gmail.com>
2019-07-14 10:02:46 -0700

    #1 Starts the Babylon scene

--- src/js/scene.js
+++ src/js/scene.js
@@ -10,12 +10,20 @@ export default class {
     this.scene = new BABYLON.Scene(this.engine)
     this.head = null
 
+    const camera = new BABYLON.ArcRotateCamera('Camera', Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0,0,5), this.scene)
+
     BABYLON.SceneLoader.Append('./3d/', 'scene.gltf', this.scene, scene => {
       this.head = scene.meshes[0]
       this.head.rotationQuaternion = null
       this.head.rotation.y = Math.PI
       scene.createDefaultCameraOrLight(true, true, true)
       scene.activeCamera.alpha += Math.PI
+
+      camera.attachControl(scene)
+    })
+
+    this.engine.runRenderLoop(() => {
+      this.scene.render()
     })
   }
 }
