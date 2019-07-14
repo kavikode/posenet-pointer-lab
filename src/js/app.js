92f411c50841ddef6e90fcf5d9514c7b0257706a
Oz Ramos <labofoz@gmail.com>
2019-07-14 11:53:44 -0700

    #1 Styles the UI a bit

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,13 @@
 import Scene from './scene'
 const $canvas = document.querySelector('canvas')
+const $startPosenet = document.querySelector('#start-posenet')
+const $startTraining = document.querySelector('#start-training')
 
+// Start a new scene
 const scene = new Scene($canvas)
+
+// Start posenet on click
+$startPosenet.addEventListener('click', function () {
+  this.disabled = true
+  $startTraining.disabled = false
+})
