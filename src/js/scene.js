fc90a90dda6687815e162e7a6a98e28ddc460462
Oz Ramos <labofoz@gmail.com>
2019-07-14 18:26:40 -0700

    #3 Prevents camera from rotating scene

--- src/js/scene.js
+++ src/js/scene.js
@@ -6,30 +6,32 @@ export default class {
    * @param {CANVAS} $canvas The HTML Canvas to contain our Babylon (and PoseNet) scene
    */
   constructor ($canvas) {
+    this.$canvas = $canvas
     this.plugins = []
     this.engine = new BABYLON.Engine($canvas, true)
     this.scene = new BABYLON.Scene(this.engine)
     this.head = null
+    this.camera = null
 
-    this.createScene($canvas)
+    this.createScene()
     this.startLoop()
   }
 
   /**
    * Creates the scene
    */
-  createScene ($canvas) {
-    const camera = new BABYLON.ArcRotateCamera('Camera', Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0,0,5), this.scene)
-
+  createScene () {
+    this.camera = new BABYLON.ArcRotateCamera('Camera', Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0,0,5), this.scene)
+    
     BABYLON.SceneLoader.Append('./3d/', 'scene.gltf', this.scene, scene => {
       this.head = scene.meshes[0]
       this.head.rotationQuaternion = null
       this.head.rotation.y = Math.PI
-      scene.createDefaultCameraOrLight(true, true, true)
+      scene.createDefaultCameraOrLight(true, true, false)
       scene.activeCamera.alpha += Math.PI
 
-      camera.attachControl(scene)
-      $canvas.style.width = '100%'
+      this.$canvas.style.width = '100%'
+      this.camera.detachControl(this.$canvas)
     })
   }
 
@@ -39,6 +41,7 @@ export default class {
   startLoop () {
     this.engine.runRenderLoop(() => {
       this.scene.render()
+      this.camera.detachControl(this.$canvas)
 
       this.plugins.forEach(plugin => {
         plugin.call(this)
@@ -54,3 +57,25 @@ export default class {
     this.plugins.push(callback)
   }
 }
+
+/**
+ * Rotate head
+ */
+// canvas.addEventListener("pointerdown", function (evt) {
+//   currentPosition.x = evt.clientX;
+//   currentPosition.y = evt.clientY;
+  
+//   clicked = true;
+// });
+
+// canvas.addEventListener("pointermove", function (evt) {
+//   if (!clicked) {
+//     return;
+//   }
+//   cylinder.rotation.y = (evt.clientX - currentPosition.x) / 10.0;
+//   cylinder.rotation.x = (evt.clientY - currentPosition.y) / 10.0;
+// });
+
+// canvas.addEventListener("pointerup", function (evt) {
+//   clicked = false;
+// });
